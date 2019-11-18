ea4fffd65570b4c26968ca080aab4c5082756ff6
RaiAnant <iit2017103@iiita.ac.in>
2019-11-18 16:32:17 +0530

    improved ui

--- app/src/main/java/com/example/clinic/adapters/ClientAdapter.kt
+++ app/src/main/java/com/example/clinic/adapters/ClientAdapter.kt
@@ -1,4 +1,75 @@
 package com.example.clinic.adapters
 
-class ClientAdapter {
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.clinic.AddMedication
+import com.example.clinic.Model.Appointment
+import com.example.clinic.Model.Doctor
+import com.example.clinic.R
+import com.google.firebase.database.FirebaseDatabase
+import java.lang.Appendable
+
+class ClientAdapter(var context: Context):
+    RecyclerView.Adapter<ClientAdapter.MyViewHolder>() {
+
+    private val appoinList = ArrayList<Appointment>()
+    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+
+    fun addItem(appointment: Appointment){
+        appoinList.add(appointment)
+        notifyItemInserted(appoinList.size-1)
+
+    }
+
+
+    class MyViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        var name: TextView = view.findViewById(R.id.textName)
+        var date: TextView = view.findViewById(R.id.textDate)
+
+
+        fun bind(appointment: Appointment, context: Context){
+            name.text = appointment.clientName
+            date.text = appointment.date
+//            val dialogClickListener =
+//                DialogInterface.OnClickListener { dialog, which ->
+//                    when (which) {
+//                        DialogInterface.BUTTON_POSITIVE -> {
+//                            firebaseDatabase.getReference("appointments/"+doc.docId).setValue(appointment)
+//                        }
+//
+//                        DialogInterface.BUTTON_NEGATIVE -> {
+//                        }
+//                    }//Yes button clicked
+//                    //No button clicked
+//                }
+            (view as CardView).setOnClickListener {
+//                var intent: Intent = Intent(context, )
+            }
+        }
+
+
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): ClientAdapter.MyViewHolder {
+
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item_appointments,parent,false))
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(appoinList[position], context)
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = appoinList.size
 }
